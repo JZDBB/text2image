9c2ae4641d6d85ba1c1610caaa42017d6b4a9bf9
XuYining <oxuyining@gmail.com>
2019-12-30 18:08:49 +0800

    init model 2

--- code/model.py
+++ code/model.py
@@ -309,6 +309,37 @@ class upfeature(nn.Module):
 
 
 # ############## G networks ###################
+class CA_NET(nn.Module):
+    # some code is modified from vae examples
+    # (https://github.com/pytorch/examples/blob/master/vae/main.py)
+    def __init__(self):
+        super(CA_NET, self).__init__()
+        self.t_dim = cfg.TEXT.EMBEDDING_DIM
+        self.c_dim = cfg.GAN.CONDITION_DIM
+        self.fc = nn.Linear(self.t_dim, self.c_dim * 4, bias=True)
+        self.relu = GLU()
+
+    def encode(self, text_embedding):
+        x = self.relu(self.fc(text_embedding))
+        mu = x[:, :self.c_dim]
+        logvar = x[:, self.c_dim:]
+        return mu, logvar
+
+    def reparametrize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        if cfg.CUDA:
+            eps = torch.cuda.FloatTensor(std.size()).normal_()
+        else:
+            eps = torch.FloatTensor(std.size()).normal_()
+        eps = Variable(eps)
+        return eps.mul(std).add_(mu)
+
+    def forward(self, text_embedding):
+        mu, logvar = self.encode(text_embedding)
+        c_code = self.reparametrize(mu, logvar)
+        return c_code, mu, logvar
+
+
 class INIT_STAGE_G(nn.Module):
     def __init__(self, ngf, ncf):
         super(INIT_STAGE_G, self).__init__()
